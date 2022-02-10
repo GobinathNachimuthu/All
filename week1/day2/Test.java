d75bff84bc958c0c725287f1d555083d9c373d4e
Gobi <98224225+GobinathNachimuthu@users.noreply.github.com>
2022-02-11 01:20:10 +0530

    Add files via upload

---
+++ week1/day2/Test.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class Test {
+	public static void main(String[] args) {
+		int num = 151;
+		boolean flag = false;
+		for (int i = 2; i <= num / 2; ++i) {
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		if (flag == false)
+			System.out.println("  IS A PRIME");
+		else
+			System.out.println(" IS NOT A PRIME");
+	}
+
+}
